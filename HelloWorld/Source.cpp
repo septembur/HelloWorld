c627df39d1077ac94cfc95e749078dd7a0c51d36
septembur <Personal@LAPTOP-PMMGD5FC>
2021-04-10 13:22:06 -0400

    this time for real?

---
+++ HelloWorld/Source.cpp
@@ -0,0 +1,14 @@
+//septembur
+//1-10-21
+//hello world
+//probably gonna edit this, arent I?
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
